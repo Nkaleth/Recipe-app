8521cfbfdac6ec05955413c6bfe71f5245f09790
hopeforkoami <hopeforkoami@gmail.com>
2023-05-25 14:47:42 +0000

    recipe controller routes and views

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
+
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   resources :users, only: [:index, :show, :create, :new] do
     resources :foods, only: [:index, :show, :create, :new]
   end
+  resources :recipes
 
   resources :foods, only: [:index]
 
