2c79c76d0ae5465e216cc1adb952aeb62fa3d81e
Nkaleth <nseguralu@gmail.com>
2023-05-24 23:08:53 -0500

    ADD routes for user > recipes

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   resources :users, only: [:index, :show, :create, :new] do
     resources :foods, only: [:index, :show, :create, :new, :destroy]
+    resources :recipes, only: [:index, :show, :create, :new, :destroy]
   end
 
   resources :foods, only: [:index]
