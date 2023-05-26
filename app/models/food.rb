4b1f517a345060f639c13235273ad4831eeb9074
Nkaleth <nseguralu@gmail.com>
2023-05-26 13:05:54 -0500

    FIX delete method in order to delete all related recipeFoods to Foods model

--- app/models/food.rb
+++ app/models/food.rb
@@ -1,5 +1,5 @@
 class Food < ApplicationRecord
   validates :price, :quantity, numericality: { greater_than_or_equal_to: 0 }
-  has_many :recipe_foods, class_name: 'RecipeFood', foreign_key: 'food_id'
+  has_many :recipe_foods, class_name: 'RecipeFood', foreign_key: 'food_id', dependent: :destroy
   belongs_to :user, class_name: 'User'
 end
