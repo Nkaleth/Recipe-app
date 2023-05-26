e3c88ebd84dde5d1b5559bfc1e10f2ca1ff5e0a2
Nkaleth <nseguralu@gmail.com>
2023-05-26 12:17:02 -0500

    ADD model test for RecipeFood model

---
+++ spec/models/recipe_food_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+RSpec.describe RecipeFood, type: :model do
+  describe 'associations' do
+    it 'belongs to recipe' do
+      recipe = Recipe.new
+      recipe_food = RecipeFood.new(recipe: recipe)
+      expect(recipe_food.recipe).to eq(recipe)
+    end
+
+    it 'belongs to food' do
+      food = Food.new
+      recipe_food = RecipeFood.new(food: food)
+      expect(recipe_food.food).to eq(food)
+    end
+  end
+end
