f038054cac8efac2116cb15497b9034d58f40639
Nkaleth <nseguralu@gmail.com>
2023-05-26 12:18:18 -0500

    FIX rubocop ofensses

--- spec/models/recipe_food_spec.rb
+++ spec/models/recipe_food_spec.rb
@@ -4,13 +4,13 @@ RSpec.describe RecipeFood, type: :model do
   describe 'associations' do
     it 'belongs to recipe' do
       recipe = Recipe.new
-      recipe_food = RecipeFood.new(recipe: recipe)
+      recipe_food = RecipeFood.new(recipe:)
       expect(recipe_food.recipe).to eq(recipe)
     end
 
     it 'belongs to food' do
       food = Food.new
-      recipe_food = RecipeFood.new(food: food)
+      recipe_food = RecipeFood.new(food:)
       expect(recipe_food.food).to eq(food)
     end
   end
