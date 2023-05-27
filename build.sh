3df41f4084aec4fc8d575553c315359c3287b9b2
Nkaleth <nseguralu@gmail.com>
2023-05-27 12:27:03 -0500

    adding the directory where user-level gems are installed to the PATH environment variable

--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 set -e
+export PATH="$(ruby -r rubygems -e 'puts Gem.user_dir')/bin:$PATH"
 gem install bundler -v 2.3.26 --user-install
 bundle install
 bundle exec rake assets:precompile
